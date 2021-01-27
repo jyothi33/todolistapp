aadc4e34608d9bd74cd938ddcc7fc8dad6746ad6
Jyothi <jyothi.raghavendra3@gmail.com>
2021-01-27 13:29:30 +0530

    Delete InputTodo file which is not used int his project.

--- src/components/InputTodo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function InputTodo(props) {
-   
-        return <input type="text" value={props.item} id={props.id} onChange={props.editTodo}></input>
-    
-}
-
-export default InputTodo
